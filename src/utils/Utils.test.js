8cb0e5e69ceacac20e33354b1a5e1aa86c2cf82e
Henrique-Arau <penielh43@gmail.com>
2024-02-21 18:01:38 -0300

    Testes de depósito e transferência

--- src/utils/Utils.test.js
+++ src/utils/Utils.test.js
@@ -1,3 +1,33 @@
+import { calculaNovoSaldo } from './index';
+
+
+
+describe('Quando eu realizo uma transação', ()=>{
+    test('Que é um deposito, o saldo deve aumentar', ()=>{
+        const transacao = {
+            transacao: 'Depósito',
+            valor: 50,
+        };
+
+        const novoSaldo = calculaNovoSaldo(transacao, 100);
+        expect(novoSaldo).toBe(150);
+    });
+
+    test('Que é um deposito, o saldo deve diminuir', ()=>{
+        const transacao = {
+            transacao: 'Transferência',
+            valor: 50,
+        };
+
+        const novoSaldo = calculaNovoSaldo(transacao, 100);
+        expect(novoSaldo).toBe(50);
+    });
+});
+
+
+
+
+
 test('Deve retornar o valor do saldo com rendimento', ()=>{
     const calcularRendimento = jest.fn(saldo => saldo + saldo * 0.005);
 
@@ -8,4 +38,4 @@ test('Deve retornar o valor do saldo com rendimento', ()=>{
     expect(novoSaldo).toBe(100.5);
     expect(calcularRendimento).toBeCalled();
     expect(calcularRendimento).toHaveBeenCalledWith(saldo);
-})
+});
