c5224c3d1d29061f7b39b32ef4a499998fb6bf03
Henrique-Arau <penielh43@gmail.com>
2024-02-20 21:55:32 -0300

    testes de metodos de consulta

---
+++ src/componentes/Menu/Menu.test.js
@@ -0,0 +1,20 @@
+import { render , screen} from '@testing-library/react'
+import Menu from './index';
+
+test('Deve renderizar um link para pagina inicial', ()=> {
+    render(<Menu/>);
+    const linkPaginaInicial = screen.getByText('Inicial');
+    expect(linkPaginaInicial).toBeInTheDocument();
+});
+
+test('Deve renderizar uma lista de links', ()=> {
+    render(<Menu/>);
+    const listaDeLinks = screen.getAllByRole('link');
+    expect(listaDeLinks).toHaveLength(4);
+});
+
+test('Não deve renderizar o link para extrato', ()=> {
+    render(<Menu/>);
+    const linkExtrato = screen.queryByText('Extrato');
+    expect(linkExtrato).not.toBeInTheDocument;
+});
